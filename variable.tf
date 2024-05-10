63e319e5594ca9a5930b3ef4f3fbf6bfc1d4193c
Ranika Williams <rnkwilliams07@gmail.com>
2024-05-10 10:23:04 -0500

    create sns

--- variable.tf
+++ variable.tf
@@ -80,5 +80,9 @@ variable "ssl_certificate_arn" {
   type        = string
 }
 
-
-
+# sns topic variables
+variable "operator_email" {
+  default     = "rnkwilliams07@gmail.com"
+  description = "a valid email address"
+  type        = string
+}
